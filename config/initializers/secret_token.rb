d362b73db5d2932bc45a5843178a8a14dc980991
Marcos Díaz Pc <marcos.luis.garcia@gmail.com>
2012-11-16 17:43:42 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeriesJump::Application.config.secret_token = '967c8e1bc7fbea34c3d1d5382c02217f5a414487e713728bc9cd52028492ba491c4315fc942350daa726be1e9caebb196fe49caa9d5180cf6663ddecb22b828d'
