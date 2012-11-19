43b25e2b1f2dbf488fa016b25f6b48b54a52b921
Alelit4 <alexandrarivero2003@hotmail.com>
2012-11-19 13:50:29 +0000

    paginas estaticas, dinamicas y enlazado bootstrap

--- Gemfile
+++ Gemfile
@@ -12,9 +12,9 @@ gem 'rails', '3.2.0'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
+  gem 'bootstrap-sass'
 
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer'
+   # gem 'therubyracer'
 
   gem 'uglifier', '>= 1.0.3'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 SeriesJump::Application.routes.draw do
+
+
   resources :series
 
   resources :peliculas
 
    resources :users
 
-   root :to => 'home#index'
+ #  root :to => 'home#index'
 
+  root to: 'static_pages#home'
+
+   
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/privacy', to: 'static_pages#privacy'
+  match '/terms', to: 'static_pages#terms'
+  
+  
 	match '/auth/:provider/callback' => 'sessions#create'
 	match '/auth/failure' => 'sessions#failure'
 	match '/signout' => 'sessions#destroy', :as => :signout
 	match '/signin' => 'sessions#new', :as => :signin
 	match '/signin1' => 'sessions#newother', :as => :signin1
+	
+	
 end
