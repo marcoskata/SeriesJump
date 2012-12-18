b6e8fee2f5df33ae8cf698d37a4eccb5ddb6858f
Alelit4 <alexandrarivero2003@hotmail.com>
2012-12-18 15:37:29 +0000

    twitter

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -2,4 +2,15 @@
 // This file is automatically included by javascript_include_tag :defaults
 
   
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+!function(d, s, id) {  
+  var js, fjs = d.getElementsByTagName(s)[0];  
+  if (d.getElementById(id)) return;  
+  js = d.createElement(s); js.id = id;  
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";  
+  fjs.parentNode.insertBefore(js, fjs);  
+}(document, 'script', 'facebook-jssdk'); 
+
+!function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}
+}(document,"script","twitter-wjs");
