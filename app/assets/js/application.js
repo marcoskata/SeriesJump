71e7d1ffdc2a56440fd7d5626547595b08849c5a
Alelit4 <alexandrarivero2003@hotmail.com>
2012-12-18 12:28:02 +0000

    mensajes twitter

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -2,3 +2,4 @@
 // This file is automatically included by javascript_include_tag :defaults
 
   
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
