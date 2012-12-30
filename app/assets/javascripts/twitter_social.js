7b925145f00f1b54d50ea20d3b92ea52aaf883cd
Alelit4 <alexandrarivero2003@hotmail.com>
2012-12-30 12:47:50 +0000

    arreglado botones social

---
+++ app/assets/javascripts/twitter_social.js
@@ -0,0 +1,13 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+ 
+!function(d,s,id){
+	var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)){
+		js=d.createElement(s); 
+		js.id=id;
+		js.src="//platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js,fjs);
+	}
+}(document,"script","twitter-wjs");
